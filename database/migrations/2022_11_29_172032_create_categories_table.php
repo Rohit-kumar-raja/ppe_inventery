eab8f6a5a68f7dca64911dfb6f45f0b92ad9d615
ajit9639 <61927091+ajit9639@users.noreply.github.com>
2022-12-02 19:08:08 +0530

    done

--- database/migrations/2022_11_29_172032_create_categories_table.php
+++ database/migrations/2022_11_29_172032_create_categories_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
+
+        
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
