e8041e201270c027feb371bded336b76d1d2396f
Raja <raja@raja.com>
2022-12-07 22:04:53 +0530

    all doen

--- database/migrations/2022_12_03_062835_create_employees_table.php
+++ database/migrations/2022_12_03_062835_create_employees_table.php
@@ -27,26 +27,17 @@ return new class extends Migration
             $table->string('maritalstatus');
             $table->date('dob');
             $table->string('placebirth');
-<<<<<<< HEAD
             $table->string('qualification');
-            $table->string('exp');
-            $table->string('lastcompany');
-
             $table->string('pan_no')->nullable();
             $table->string('adhar_no')->nullable();
             $table->string('bank_name')->nullable();
             $table->string('back_act_num')->nullable();
-            
-
-=======
-            $table->string('qualification')->nullable();
             $table->string('exp')->default(0);
             $table->string('lastcompany')->nullable();
->>>>>>> 1930c2bf03f4874fa9459255f179e10ad4373c49
-            $table->string('photo')->nullable();       ;
-            $table->string('adhar')->nullable();       ;
-            $table->string('pan')->nullable();       ;
-            $table->string('passbook')->nullable();       ;
+            $table->string('photo')->nullable();       
+            $table->string('adhar')->nullable();       
+            $table->string('pan')->nullable();       
+            $table->string('passbook')->nullable();      
             $table->boolean('status')->default(1);
             $table->foreign('designation_id')->references('id')->on('designations');
             $table->foreign('area_id')->references('id')->on('areas');
